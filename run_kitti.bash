dbf5875afd10fc5d5e6419afca653a6cb0506d19
ShreyasSkandanS <sshreyas@seas.upenn.edu>
2022-02-15 15:24:06 -0500

    fixed all calibs upto 10

--- run_kitti.bash
+++ run_kitti.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #usage: python xxx.py file_name
 
-for((i=00;i<=00;i++))
+for((i=07;i<=07;i++))
   do
     {
      a=$((100+$i))
@@ -12,10 +12,10 @@ for((i=00;i<=00;i++))
     # run dso
      ./build/bin/dso_dataset \
  	  files=/home/shreyas/Work/kitti/dataset/sequences/${seqnum} \
- 	  calib=/home/shreyas/Work/kitti/dataset/sequences/${seqnum}/calib.txt \
+ 	  calib=/home/shreyas/stereo_DSO/calib/kitti/${seqnum}.txt \
 	  groundtruth=/home/shreyas/Work/kitti/dataset/poses/${seqnum}.txt \
  	  preset=0 mode=1 \
-	  quiet=1 nomt=1
+	  quiet=1 nomt=1 nogui=1
     }&
   done
 
