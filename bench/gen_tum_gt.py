47f81b40ec47efdfb80f248dd0e37a65fd5b2101
ShreyasSkandanS <sshreyas@seas.upenn.edu>
2022-02-17 19:52:40 -0500

    Ian calib conversion code updated

--- bench/gen_tum_gt.py
+++ bench/gen_tum_gt.py
@@ -3,7 +3,7 @@
 from scipy.spatial.transform import Rotation
 import numpy as np
 
-basedir = '/media/ian/HDD1/KITTI/dataset/poses'
+basedir = '/home/shreyas/Work/kitti/dataset/poses'
 seqs = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 
 for seq in seqs:
@@ -11,7 +11,7 @@ for seq in seqs:
     outfile = open('KITTI_dataset_sequences_' + seq + '.txt', 'w')
     for ind, pose_str in enumerate(file.readlines()):
         raw = np.fromstring(pose_str, sep=' ').reshape(3, 4)
-        R = Rotation.from_dcm(raw[:3, :3]).as_quat()
+        R = Rotation.from_matrix(raw[:3, :3]).as_quat()
         outfile.write(f"{ind} {raw[0,3]} {raw[1,3]} {raw[2,3]} {R[0]} {R[1]} {R[2]} {R[3]}\n")
     outfile.close()
 
