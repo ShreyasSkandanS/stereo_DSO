d74a0f7dab3d8d6de2527ee93956a0381c6464dd
Shreyas Skandan <sshreyas@seas.upenn.edu>
2022-02-20 16:37:12 -0500

    updated dsol bench for all three datasets

--- bench/dsol_bench.py
+++ bench/dsol_bench.py
@@ -47,26 +47,27 @@ class DsoRun:
                 retries += 1
 
 class DsolRun(DsoRun):
-    def __init__(self, dataset, dataset_calib, gt, reverse=False):
+    def __init__(self, dataset, dataset_calib, gt, reverse, dataset_id):
         super().__init__(reverse)
         self.method_ = 'dsol'
         self.dataset_ = dataset
         self.reverse_ = reverse
-        self.cmd_ = ['roslaunch', 'svcpp', 'dsol_data.launch', 'save:=' + str(Path.cwd()) + '/result.txt', 'base_dir:=' + dataset]
+        self.dataset_id_ = dataset_id
+        self.cmd_ = ['roslaunch', 'svcpp', 'dsol_data.launch', 'save:=' + str(Path.cwd()) + '/result.txt', 'data_dir:=' + dataset, 'data:=' + dataset_id]
         #if self.reverse_:
         #    self.cmd_.append('
 
 class DsolBench:
     def __init__(self):
-        self.datasets_ = yaml.safe_load(open('datasets_vkitti.yml', 'r'))
+        self.datasets_ = yaml.safe_load(open('datasets/datasets_kitti.yml', 'r'))
     
     def run(self):
         for dataset in self.datasets_:
             for seq in dataset['sequences']:
                 full_path = dataset['root'] + '/' + seq
-                dr = DsolRun(full_path, dataset['calib'], dataset['gt'])
+                dr = DsolRun(full_path, dataset['calib'], dataset['gt'], False, dataset['dataset'])
                 dr.run()
-                #dr = DsolRun(full_path, dataset['calib'], dataset['gt'], True)
+                #dr = DsolRun(full_path, dataset['calib'], dataset['gt'], True, dataset['dataset'])
                 #dr.run()
 
 if __name__ == '__main__':
