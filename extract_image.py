b8f3170c33a06acbbc1500176c268edbd0372f02
chao <quchao@seas.upenn.edu>
2022-02-28 14:30:00 -0500

    update extract

--- extract_image.py
+++ extract_image.py
@@ -7,7 +7,9 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 if __name__ == '__main__':
-    bag = rosbag.Bag("/home/chao/Dropbox/20220227_211624.bag", "r")
+    bag_file = "/home/chao/Dropbox/Data/20220227_164341.bag"
+    bag_name = "outdoor_slow"
+    bag = rosbag.Bag(bag_file, "r")
     bridge = CvBridge()
     count = 0
     topics = [
@@ -15,18 +17,19 @@ if __name__ == '__main__':
         "/device_0/sensor_0/Infrared_2/image/data"
     ]
 
-    data_dir = Path("/home/chao/Documents/realsense")
+    data_dir = Path("/home/chao/Documents/realsense") / bag_name
     left_dir = data_dir / "infra1"
     right_dir = data_dir / "infra2"
     left_dir.mkdir(parents=True, exist_ok=True)
     right_dir.mkdir(parents=True, exist_ok=True)
 
     for topic, msg, t in bag.read_messages(topics=topics):
+        count = t.to_nsec() // 1000000
+        print(count)
+
         cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         if topic == topics[0]:
-            cv2.imwrite(str(left_dir / f"image{count:06d}.png"), cv_img)
-            count += 1
-            print(count)
+            cv2.imwrite(str(left_dir / f"image{count:08d}.png"), cv_img)
         elif topic == topics[1]:
-            cv2.imwrite(str(right_dir / f"image{count:06d}.png"), cv_img)
+            cv2.imwrite(str(right_dir / f"image{count:08d}.png"), cv_img)
     bag.close()
