0be243bc86186112d98c3fac9e7f69560705e945
chao <quchao@seas.upenn.edu>
2022-02-28 14:31:46 -0500

    add run realsense

--- run_single_realsense.bash
+++ run_single_realsense.bash
@@ -3,8 +3,7 @@
 
 # run dso
 ./build/bin/dso_dataset \
-    files=/home/chao/Documents/realsense \
+    files=/home/chao/Documents/realsense/outdoor_fast \
     calib=/home/chao/Workspace/repo/versatran01/sdso/calib/realsense/calib.txt \
     groundtruth=/home/chao/Workspace/repo/versatran01/sdso/groundTruthPose/dummy.txt \
-    preset=0 mode=1 \
-    quiet=1 nomt=0
+    mode=1 quiet=1 nomt=0 nolog=1 nogui=0 preset=0
